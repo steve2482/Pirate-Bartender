3cf037867963c89ea6a841372ca2b2193d24c2e0
steve2482 <stevecb12684@yahoo.com>
2016-10-11 20:36:43 -0400

    UI functional, drink preferences recorded

--- apps/bartender.js
+++ apps/bartender.js
@@ -1,31 +1,27 @@
 
 // Bartender questions constructor function
 
-function Questions(strong, salty, bitter, sweet, fruity) {
-  this.strong = strong;
-  this.salty = salty;
-  this.bitter = bitter;
-  this.sweet = sweet;
-  this.fruity = fruity;
+function Questions() {
+  this.questions = ['Do ye like yer drinks strong?', 'Do ye like it with a salty tang?', 'Are ye a lubber who likes it bitter?', 'Would ye like a bit of sweetness with yer poison?', 'Are ye one for a fruity finish?'];
 }
 
 // Bartender questions
 
-var barQuestions = new Questions('Do ye like yer drinks strong?', 'Do ye like it with a salty tang?', 'Are ye a lubber who likes it bitter?', 'Would ye like a bit of sweetness with yer poison?', 'Are ye one for a fruity finish?');
+var barQuestions = new Questions();
 
 // Bar ingredients contructor function
 
-function Ingredients(strong, salty, bitter, sweet, fruity) {
-  this.strong = strong;
-  this.salty = salty;
-  this.bitter = bitter;
-  this.sweet = sweet;
-  this.fruity = fruity;
+function Ingredients() {
+  this.strong = ['glug of rum', 'slug of whisky', 'splash of gin'];
+  this.salty = ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'];
+  this.bitter = ['shake of bitters', 'splash of tonic', 'twist of lemon peel'];
+  this.sweet = ['sugar cube', 'spoonful of honey', 'spash of cola'];
+  this.fruity = ['slice of orange', 'dash of cassis', 'cherry on top'];
 }
 
 // Bar ingredients
 
-var barIngredients = new Ingredients(['glug of rum', 'slug of whisky', 'splash of gin'], ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'], ['shake of bitters', 'splash of tonic', 'twist of lemon peel'], ['sugar cube', 'spoonful of honey', 'spash of cola'], ['slice of orange', 'dash of cassis', 'cherry on top']);
+var barIngredients = new Ingredients();
 
 // Pantry constructor function
 
@@ -33,25 +29,40 @@ function Pantry(barIngredients) {
   this.barIngredients = barIngredients;
 }
 
-// Resturant Pantry
+// Resturant Pantry for when adding food questions later
 
 var resturantPantry = new Pantry(barIngredients);
 
-function Preferences(strong, salty, bitter, sweet, fruity) {
-  this.strong = strong;
-  this.salty = salty;
-  this.bitter = bitter;
-  this.sweet = sweet;
-  this.fruity = fruity;
+// Customer preferences constructor function
+
+function Preferences() {
+  this.preferences = [];
 }
 
 $(document).ready(function() {
+  var customerPreferences = new Preferences();
+
   // User answers if they want a drink yes/no
 
-  $('#yes').click(function(e) {
+  $('#need').click(function(e) {
     e.preventDefault();
-    $('.bar-questions').text(barQuestions.strong);
-    $('#yes').text('Yes');
-    $('#no').text('No');
+    $('.bar-questions').text(barQuestions.questions[0]);
+    $('.drink-buttons').hide();
+    $('.answer-buttons').show();
+  });
+
+  // Answer to drink preferences
+
+  var questionNumber = 0;
+  $('#yes, #no').click(function(e) {
+    e.preventDefault();
+    if (this.id === 'yes') {
+      customerPreferences[questionNumber] = 'yes';
+    } else if (this.id === "no") {
+      customerPreferences[questionNumber] = "no";
+    }
+    questionNumber++;
+    $('.bar-questions').text(barQuestions.questions[questionNumber]);
+    console.log(customerPreferences);
   });
 });
