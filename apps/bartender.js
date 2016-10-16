f465aab18bfe221b6ced3ca39a5dbe51ab85af45
steve2482 <stevecb12684@yahoo.com>
2016-10-16 19:15:30 -0400

    Drink mix displayed after all preferences have been made

--- apps/bartender.js
+++ apps/bartender.js
@@ -5,35 +5,22 @@ function Questions() {
   this.questions = ['Do ye like yer drinks strong?', 'Do ye like it with a salty tang?', 'Are ye a lubber who likes it bitter?', 'Would ye like a bit of sweetness with yer poison?', 'Are ye one for a fruity finish?'];
 }
 
-// Bartender questions
-
-var barQuestions = new Questions();
-
-// create the Pirate Bartender
-
-var pirateBartender = new Bartender();
-
 // Bar ingredients constructor function
 
-var randomIngredient = Math.floor((Math.random() * 2) + 0);
 function Ingredients() {
-  this.ingredients = [{strong: ['glug of rum', 'slug of whisky', 'splash of gin']}, {salty: ['olive on a stick', 'salt-dusted rim', 'rasher of bacon']}, {bitter: ['shake of bitters', 'splash of tonic', 'twist of lemon peel']}, {sweet: ['sugar cube', 'spoonful of honey', 'spash of cola']}, {fruity: ['slice of orange', 'dash of cassis', 'cherry on top']}];
-  this.randomIngredient = randomIngredient;
+  this.ingredients = [['glug of rum', 'slug of whisky', 'splash of gin'],
+  ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+  ['shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+  ['sugar cube', 'spoonful of honey', 'spash of cola'],
+  ['slice of orange', 'dash of cassis', 'cherry on top']];
+  this.randomIngredient = Math.floor((Math.random() * 3) + 0);
 }
-
-// Bar ingredients
-
-var barIngredients = new Ingredients();
 
 // Pantry constructor function
 
 function Pantry(barIngredients) {
-  this.barIngredients = barIngredients;
+  this.barIngredients = new Ingredients();
 }
-
-// Restaurant Pantry for when adding food questions later
-
-var restaurantPantry = new Pantry(barIngredients);
 
 // Customer preferences constructor function
 
@@ -44,12 +31,12 @@ function Preferences() {
 // Bartender constructor function
 
 function Bartender() {
-  this.questions = barQuestions;
+  var restaurantPantry = new Pantry();
   this.createDrink = function(preferences) {
     var drink = [];
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < preferences.length; i++) {
       if (preferences[i]) {
-        drink.push(barIngredients[i].randomIngredient);
+        drink.push(restaurantPantry.barIngredients.ingredients[i][restaurantPantry.barIngredients.randomIngredient]);
       }
     }
     return drink;
@@ -60,7 +47,8 @@ function Bartender() {
 
 $(document).ready(function() {
   var customerPreferences = new Preferences();
-
+  var barQuestions = new Questions();
+  
   // User answers if they want a drink yes/no
 
   $('#need').click(function(e) {
@@ -76,11 +64,11 @@ $(document).ready(function() {
   $('#yes, #no').click(function(e) {
     e.preventDefault();
     if (this.id === 'yes') {
-      customerPreferences[questionNumber] = true;
+      customerPreferences.preferences[questionNumber] = true;
       questionNumber++;
       $('.bar-questions').text(barQuestions.questions[questionNumber]);
-    } else if (this.id === "no") {
-      customerPreferences[questionNumber] = false;
+    } else if (this.id === 'no') {
+      customerPreferences.preferences[questionNumber] = false;
       questionNumber++;
       $('.bar-questions').text(barQuestions.questions[questionNumber]);
     }
@@ -91,10 +79,17 @@ $(document).ready(function() {
     }
   });
 
-// Need to create event listener-----------------
-  $('.create').click(function(e) {
+  $('#create').click(function(e) {
     e.preventDefault();
-    pirateBartender.createDrink(customerPreferences);
-    console.log(drink);
+    var pirateBartender = new Bartender();
+    var drink = pirateBartender.createDrink(customerPreferences.preferences);
+    var last = drink.length - 1;
+    $('.bar-questions').text("Here's your drink mixed with");
+    for (var i = 0; i < drink.length - 1; i++) {
+      $('.bar-questions').append(", a " + drink[i].toString());
+    }
+    $('.bar-questions').append(" and a " + drink[last]);
+    $('.bar-questions').append(".");
+    $('.create-drink').hide();
   });
 });
